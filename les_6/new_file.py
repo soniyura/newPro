a1a91f6c4cd975cd16b6bb8c723694961757c310
soniyura <33290862+soniyura@users.noreply.github.com>
2023-12-06 17:05:16 +0200

    7 les calc_quadr_eq

--- les_6/new_file.py
+++ les_6/new_file.py
@@ -11,6 +11,7 @@ def input_data() -> List[List[int]]:
     List_a_b_c_int = [list_a_int, list_b_int, list_c_int]
     return List_a_b_c_int
 
+
 def validate_input(raw_data: List[List[int]]) -> bool:
     """
     case 1: empty list
@@ -21,29 +22,51 @@ def validate_input(raw_data: List[List[int]]) -> bool:
     """
     for l in raw_data:
         if not l:
-            return False # case 1
+            return False       # case 1
 
     if len(raw_data[0]) != len(raw_data[1]) != len(raw_data[2]):
-        return False  # case 2
+        return False           # case 2
 
     for el in raw_data[0]:
         if el == 0:
-            return False   # case 3
+            return False       # case 3
 
     return True
 
-
-
-
+"""
+valid_dta = [
+    [1, 2, 3], a
+    [2, 3, 4], b
+    [-1, -1, -1] c
+    ]
+    
+result = [
+    (x1, x2) zero
+    (x1, x2) first
+    (x1, x2) second
+    ]
+"""
 
 def calc_quadr_eq(valid_data: List[List[int]]) -> List[Tuple[Union[str, int]]]:
-    pass
+    for ind, el in enumerate(valid_data[0]):
+        a = el
+        b = valid_data[1][ind]
+        c = valid_data[2][ind]
+        print(f'Here is a = {a}')
+        print(f'Here is b = {b}')
+        print(f'Here is c = {c}')
+        print()
+        print(f'{a}x^2 + {b}x + {c} = 0')
+
+
+
 
 def main():
     input_list = input_data()
     print(input_list)
     if validate_input(input_list):
         calc_quadr_eq(input_list)
+
 
 if __name__ == '__main__':
     main()
